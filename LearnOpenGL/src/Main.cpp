e0aff0dced7554e6cdfff5b6503e8173bbf12eff
Nicholas <nicholascharrisofficial@outlook.com>
2024-05-16 16:39:55 -0500

    Exercises Complete
    1: Vertex Shader changes triangle orientation.
    
    2: Vertex Shader moves triangle.
    
    3: Vertex and Fragment shader move and recolor the triangle based on position.

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -15,30 +15,30 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main() {
 
-	#pragma region Create Window
-		// Initialize and configure
-		glfwInit();
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#pragma region Create Window
+	// Initialize and configure
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-		// Window Creation
-		GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-		if (window == NULL) {
-			std::cout << "Failed to create GLFW window" << std::endl;
-			glfwTerminate();
-			return -1;
-		}
+	// Window Creation
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
-		glfwMakeContextCurrent(window);
-		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-		// GLAD: Load all function pointers
-		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-			std::cout << "Failed to initialize GLAD" << std::endl;
-			return -1;
-		}
-	#pragma endregion
+	// GLAD: Load all function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+#pragma endregion
 
 	Shader ourShader("C:/dev/cpp/LearnOpenGL/LearnOpenGL/src/shaders/shader.vert", "C:/dev/cpp/LearnOpenGL/LearnOpenGL/src/shaders/shader.frag");
 
@@ -82,7 +82,7 @@ int main() {
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -90,11 +90,15 @@ int main() {
 	glBindVertexArray(0);
 #pragma endregion
 
+	float hOffsetValue = 0;
+	float vOffsetValue = 0;
+	bool moveRight = true;
+
 	// Render Loop
 	while (!glfwWindowShouldClose(window)) {
 		// Proccess Input
 		proccessInput(window);
-		
+
 		// Render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -108,12 +112,47 @@ int main() {
 		//int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
 		//glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
+		ourShader.setFloat("hOffset", hOffsetValue);
+		//ourShader.setFloat("vOffset", vOffsetValue);
+
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
-		
+
+		if (moveRight) {
+
+			if (hOffsetValue <= 0.5f) {
+				hOffsetValue += 0.005f;
+			}
+			else {
+				moveRight = false;
+			}
+			if (vOffsetValue <= 0.5f) {
+				vOffsetValue += 0.005f;
+			}
+
+		}
+		else {
+			if (hOffsetValue >= -0.5f) {
+				hOffsetValue -= 0.005f;
+			}
+			else
+			{
+				moveRight = true;
+			}
+			if (vOffsetValue >= -0.5f) {
+				vOffsetValue -= 0.005f;
+			}
+			else
+			{
+				moveRight = true;
+			}
+		}
+		std::cout << hOffsetValue << std::endl;
+		std::cout << vOffsetValue << std::endl;
+
 		// Swap Buffers and Pull IO Events
 		glfwSwapBuffers(window);
 		glfwPollEvents();
